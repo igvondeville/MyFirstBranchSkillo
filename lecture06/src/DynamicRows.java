42ec792b69010bb2eda39a60c902f370f44e6857
igvondeville <taherivanna@gmail.com>
2024-08-20 19:44:43 +0300

    Lecture 06

---
+++ lecture06/src/DynamicRows.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DynamicRows {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // int[][] matrix = {
+        // {1, 2, 3}, First row
+        // {4, 5, 6}, Second row
+        // {7, 8, 9}, Third row
+        // }
+
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.println("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+        int[][] matrix = new int[rows][columns];
+
+        for (int row = 0; row < rows; row++) {
+            System.out.println("Row: " + matrix[row]);
+        }
+    }
+}
