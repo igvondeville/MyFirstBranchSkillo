026e44f5275d7f3fbe0b430448a6ff7bef48baf0
igvondeville <taherivanna@gmail.com>
2024-08-27 22:31:18 +0300

    Lecture 06

--- src/Convert.java
+++ src/Convert.java
@@ -7,12 +7,12 @@ public class Convert {
 
     public static void convertMinutesToYearsDays() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the number of minutes");
+        System.out.println("Input the number of minutes: ");
         int minutes = scanner.nextInt();
         int days = minutes / 1440;
         int years = minutes / 525600;
 
-        System.out.println(days + "days");
-        System.out.println(years + "years");
+        System.out.println(days + " days");
+        System.out.println(years + " years");
     }
 }
