8de9de610b879f194c4cf9c92421f2d77e0d6b27
igvondeville <taherivanna@gmail.com>
2024-08-28 22:55:54 +0300

    Lecture 06

---
+++ src/BookStack.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+public class BookStack {
+    public static void main(String[] args) {
+       Stack<String> stack = new Stack<>();
+
+       stack.push("The Great Gatsby");
+       stack.push("Alice in Wonderland");
+       stack.push("The Little Prince");
+       System.out.println("Your top three books is: " + stack);
+
+       System.out.println("Your last book is: " + stack.peek());
+
+       while (!stack.isEmpty()) {
+           System.out.println("Removed book: " + stack.pop());
+       }
+if (stack.isEmpty()) {
+    System.out.println("All books have been read. Great job! ");
+}
+       }
+    }
