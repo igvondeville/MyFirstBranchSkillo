9e77f86dd7537ef8f542d434de0dd95fd2f3ad8f
igvondeville <133575679+igvondeville@users.noreply.github.com>
2024-08-31 03:39:50 +0300

    Update LargestNum.java

---
+++ lecture05/src/LargestNum.java
@@ -0,0 +1,12 @@
+public class LargestNum {
+    public static void main(String[] args) {
+        int [] numbers = {99, 33, 32, 76, 91};
+        int largestNum = 0;
+        for (int num : numbers) {
+            if (num > largestNum) {
+                largestNum = num;
+                System.out.println("The largest num is " + largestNum);
+            }
+                }
+            }
+        }
